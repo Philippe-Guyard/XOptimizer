14d0fe66914d879e603fd3606002e37ae551e811
johannaulin <johmejl@gmail.com>
2021-11-08 14:13:38 +0100

    Interaction service added to app

---
+++ app/Interaction_service/Interaction_service.cpp
@@ -0,0 +1,31 @@
+#include "Interaction_service.h"
+#include "ui_interaction_service.h"
+
+#include <Utils/Parser/pbf-parser.h>
+#include <Utils/Parser/pbf-parser.cpp>
+#include <Utils/Downloader/Downloader.cpp>
+
+
+//DOWNLOAD FUNCTIONS
+bool is_downloader_data_valid(const std::string &region, const std::string &department, const QString & filename){
+    //Check that region exists:
+
+    //Check that department exists
+
+    //Check that filename is valid.
+
+    };
+//IF TRUE ALLOWED TO CALL DOWNLOAD ELSE ERROR.
+
+
+//PARSE FUNCTIONS
+bool is_parse_data_valid(){
+    //make sure that the input stream is of a valid kid, god knows how
+};
+
+
+
+
+
+
+
---
+++ app/Interaction_service/Utils/Parser/pbf-parser.h
@@ -0,0 +1,34 @@
+//
+// Created by Nykyta Ostapliuk on 04.11.2021 AD.
+//
+
+#ifndef XOPTIMIZER_PBF_PARSER_H
+#define XOPTIMIZER_PBF_PARSER_H
+
+#define XOPTIMIZER_PBF_PARSER_DEBUG
+#ifdef XOPTIMIZER_PBF_PARSER_DEBUG
+#define XOPTIMIZER_PBF_PARSER_ASSERT(x) if (!(x)) throw std::runtime_error(#x " is not true");
+#else
+#define XOPTIMIZER_PBF_PARSER_ASSERT(x)
+#endif
+
+#include <memory>
+#include <string>
+#include <exception>
+#include <cstdint>
+#include <optional>
+#include <vector>
+#include "generated/fileformat.pb.h"
+#include "generated/osmformat.pb.h"
+
+namespace XOptimizer::PBFParser {
+    class PBFParser {
+        std::shared_ptr<std::istream> inputStream;
+    public:
+        explicit PBFParser(std::shared_ptr<std::istream> inputStream);
+        [[nodiscard]] std::shared_ptr<std::istream> getInputStream() const;
+        void parse();
+    };
+}
+
+#endif //XOPTIMIZER_PBF_PARSER_H
