a11baa6ee79cf530f666992dcd5127f51824c334
MegumiTadokoro <cosmology.constant.1915@gmail.com>
2021-11-13 11:29:55 +0100

    Minor changes to disjointset.cc

--- core-algo/test/disjointset.cc
+++ core-algo/test/disjointset.cc
@@ -5,6 +5,7 @@
 #include <vector>                   // std::vector
 #include <random>                   // std::mt19937_64
 #include <chrono>                   // std::chrono::system_clock::now().time_since_epoch().count()
+#include <algorithm>                // std::swap
 
 // External libraries
 #include "gtest/gtest.h"
@@ -17,55 +18,58 @@
  */
 class DisjointSetTest
 {
-    std::vector<int> parents;
-    
-    /**
-     * Initialize DisjointSetTest
-     * @param  {int}     number_of_elements : Number of elements.
-     */
-    DisjointSetTest(
-        int number_of_elements = 0)
-    {
-        parents = std::vector<int>(number_of_elements, -1);
-    }
-    
-    /**
-     * Find the representation of the set containing an element.
-     * @param  {int}                element : Element to query.
-     */
-    int find(
-        int element)
-    {
-        return parents[element] < 0 ? element : parents[element] = find(parents[element]);
-    }
-
-    /**
-     * Uniting two sets containing first and second, respectively
-     * @param  {int}                  first : First element.
-     * @param  {int}                 second : Second element. 
-     */
-    void union(
-        int first,
-        int second)
-    {
-        int first_root = find(first), 
-            second_root = find(second);
-        if (first_root != second_root)
+        public:
+        std::vector<int> parents;
+        
+        /**
+         * Initialize DisjointSetTest
+         * @param  {int}         number_of_elements : Number of elements.
+         */
+        DisjointSetTest(
+            int number_of_elements = 0)
         {
-            if (parents[first_root] >= parents[second_root])
-            {
-                swap(first_root, second_root);
-            }
-            parents[first_root] += parents[second_root];
-            parents[second_root] = first_root;
+            parents = std::vector<int>(number_of_elements, -1);
         }
-    }
+        
+        /**
+         * Find the representation of the set containing an element.
+         * @param  {int}                    element : Element to query.
+         * 
+         * @return {int} The representation of the set containing the queried element.
+         */
+        int find(
+            int element)
+        {
+            return parents[element] < 0 ? element : parents[element] = find(parents[element]);
+        }
+
+        /**
+         * Uniting two sets containing first and second, respectively
+         * @param  {int}                      first : First element.
+         * @param  {int}                     second : Second element. 
+         */
+        void union_set(
+            int first,
+            int second)
+        {
+            int first_root = find(first), 
+                second_root = find(second);
+            if (first_root != second_root)
+            {
+                if (parents[first_root] >= parents[second_root])
+                {
+                    std::swap(first_root, second_root);
+                }
+                parents[first_root] += parents[second_root];
+                parents[second_root] = first_root;
+            }
+        }
 };
 
 namespace
 {
     const int NUMBER_OF_NODES = 500, 
-              NUMBER_OF_QUERIES = 750,
+              NUMBER_OF_QUERIES = 750, 
               NUMBER_OF_TESTS = 20;
     std::mt19937_64 random_seed(std::chrono::system_clock::now().time_since_epoch().count());
 
@@ -76,14 +80,14 @@ namespace
 
     TEST(DisjointSet, maintest)
     {
-        for (int test = 0; test < NUMBER_OF_TEST; test++)
+        for (int test = 0; test < NUMBER_OF_TESTS; test++)
         {
             // Random number generator
             long long seed = random_seed();
             std::mt19937_64 rng(seed);
 
             DisjointSet dsu = DisjointSet(NUMBER_OF_NODES);
-            DisjointSetTest dsu_test = DisjointSetTest(NUMBER_OF_TEST);
+            DisjointSetTest dsu_test = DisjointSetTest(NUMBER_OF_NODES);
 
             for (int query_index = 0; query_index < NUMBER_OF_QUERIES; query_index++)
             {
@@ -91,8 +95,8 @@ namespace
                 {
                     int u = rng()%NUMBER_OF_NODES,
                         v = rng()%NUMBER_OF_NODES;
-                    dsu.union(u, v);
-                    dsu_test.union(u, v);
+                    dsu.Union(u, v);
+                    dsu_test.union_set(u, v);
                 }
                 else
                 {
