71444c14fa30a8d613cc6f3d4a18890dc8f8f54e
MegumiTadokoro <cosmology.constant.1915@gmail.com>
2021-11-13 13:57:24 +0100

    Update Edge tests following new interface

--- core-algo/test/edge.cc
+++ core-algo/test/edge.cc
@@ -22,15 +22,16 @@ namespace
     VertexData location_ecole_polytechnique = VertexData(coordinate_ecole_polytechnique);
     VertexData location_ens_ulm = VertexData(coordinate_ens_ulm);
 
-    std::pair<Vertex*, Vertex*>  vertices = {*coordinate_ecole_polytechnique, *coordinate_ens_ulm};
-    std::pair<Vertex*, Vertex*>  new_vertices = {*coordinate_ens_ulm, *coordinate_ecole_polytechnique};
+    Vertex ecole_polytechnique = Vertex(location_ecole_polytechnique, 0);
+    Vertex ens_ulm = Vertex(location_ens_ulm, 1);
+    std::pair<Vertex*, Vertex*>  vertices = {&ecole_polytechnique, &ens_ulm};
     
     EdgeWeight weight = 3.14159265358979;
     EdgeWeight new_weight = 2.71828182845904;
 
     TEST(Edge, Constructor)
     {
-        Edge edge = Edge(index, vertices, weight);
+        Edge edge = Edge(vertices, weight, index);
     }
     
     TEST(Edge, DefaultConstructor)
@@ -40,7 +41,7 @@ namespace
 
     TEST(Edge, GetWeight)
     {
-        Edge edge = Edge(index, vertices, weight);
+        Edge edge = Edge(vertices, weight, index);
         EdgeWeight get_weight = edge.get_weight();
         EXPECT_EQ(weight, get_weight)
             << "Initial weight and stored weight differ.\n";
@@ -48,7 +49,7 @@ namespace
 
     TEST(Edge, GetVertices)
     {
-        Edge edge = Edge(index, vertices, weight);
+        Edge edge = Edge(vertices, weight, index);
         std::pair<Vertex*, Vertex*>  get_vertices = edge.get_vertices();
         EXPECT_EQ(get_vertices.first, vertices.first)
             << "Initial vertices and stored vertices differ in the first component.\n";
@@ -58,7 +59,7 @@ namespace
 
     TEST(Edge, GetIndex)
     {
-        Edge edge = Edge(index, vertices, weight);
+        Edge edge = Edge(vertices, weight, index);
         int get_index = edge.get_index();
         EXPECT_EQ(index, get_index)
             << "Initial index and stored index differ.\n";
@@ -66,27 +67,16 @@ namespace
 
     TEST(Edge, SetWeight)
     {
-        Edge edge = Edge(index, vertices, weight);
+        Edge edge = Edge(vertices, weight, index);
         edge.set_weight(new_weight);
         EdgeWeight get_weight = edge.get_weight();
         EXPECT_EQ(new_weight, get_weight)
             << "Set weight and stored weight differ.\n";
     }
 
-    TEST(Edge, SetVertices)
-    {
-        Edge edge = Edge(index, vertices, weight);
-        edge.set_vertices(new_vertices);
-        std::pair<Vertex*, Vertex*>  get_vertices = edge.get_vertices();
-        EXPECT_EQ(get_vertices.first, new_vertices.first)
-            << "Set vertices and stored vertices differ in the first component.\n";
-        EXPECT_EQ(get_vertices.second, new_vertices.second)
-            << "Set vertices and stored vertices differ in the first component.\n";
-    }
-
     TEST(Edge, SetIndex)
     {
-        Edge edge = Edge(index, vertices, weight);
+        Edge edge = Edge(vertices, weight, index);
         edge.set_index(new_index);
         int get_index = edge.get_index();
         EXPECT_EQ(new_index, get_index)
