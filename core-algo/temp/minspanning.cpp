2c017008c5df6b9abc39b20221000f9a7b201b7f
gcognata <giuseppe.cognac@gmail.com>
2021-11-15 13:19:07 +0100

    Update minspanning.cpp

--- core-algo/temp/minspanning.cpp
+++ core-algo/temp/minspanning.cpp
@@ -10,11 +10,14 @@ std::vector<Edge*> Graph::min_spanning(){
     std::vector<Edge*> out;
     DisjointSet set = DisjointSet(num_vertices);
     int index = 0;
+    int x, y;
+    Edge* next;
+    std::pair<Vertex*, Vertex*> source_dest;
     while (out.size() != num_vertices - 1){
-        Edge* next = edges[index++];
-        std::pair<Vertex*, Vertex*> source_dest = next->get_vertices();
-        int x = set.find(source_dest.first->get_index());
-        int y = set.find(source_dest.second->get_index());
+        next = edges[index++];
+        source_dest = next->get_vertices();
+        x = set.find(source_dest.first->get_index());
+        y = set.find(source_dest.second->get_index());
         if (x != y){
             out.push_back(next);
             set.Union(x, y);
