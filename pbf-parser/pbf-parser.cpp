b8ec107b9ecfea91df25e1711628de37aa126b4c
Pylyr <avadakidabra.magic@gmail.com>
2021-11-17 01:30:43 +0100

    Revert "Merge branch 'pbf_parser' of https://github.com/Philippe-Guyard/XOptimizer into pbf_parser"
    This reverts commit 26783e0e6d0934f06a016a301efee96f5996fe8c, reversing
    changes made to 8f064f6308090e75b244cef88ba132d386cf7876.

--- pbf-parser/pbf-parser.cpp
+++ pbf-parser/pbf-parser.cpp
@@ -17,8 +17,8 @@ std::shared_ptr<std::istream> PBFParser::getInputStream() const {
     return inputStream;
 }
 
-std::shared_ptr<PBFFile> PBFParser::parse() {
-    /*std::vector<OSMPBF::BlobHeader> headers;
+void PBFParser::parse() {
+    std::vector<OSMPBF::BlobHeader> headers;
     std::vector<OSMPBF::Blob> blobs;
     char* buffer = (char*)malloc(32 * 1024 * 1024);
     for (int k = 0; inputStream->good(); k++) {
@@ -62,7 +62,6 @@ std::shared_ptr<PBFFile> PBFParser::parse() {
             std::memcpy(copy, buffer, blob.raw_size());
             rawBlobs.push_back(copy);
         }
-    }*/
-    return {};
+    }
 }
 
