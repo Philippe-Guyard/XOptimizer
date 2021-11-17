72a4f2709e019cefb3318c3701a1d6e4ea80ff1c
AntonioDaSilva <mertunsal1905@gmail.com>
2021-11-17 17:25:44 +0300

    perfect matching v2

--- core-algo/temp/matching.cpp
+++ core-algo/temp/matching.cpp
@@ -1,30 +1,24 @@
 #include "graph.hpp"
 #include "../lib/blossom5-v2_05/PerfectMatching.h"
 
-std::vector<std::pair<int,int>> Graph::perfect_mincost_matching(std::vector<Vertex*> vertices){
+std::vector<std::pair<int,int>> Graph::perfect_mincost_matching(std::vector<int> vertex_indices){
 
 
-    int n_vertices = vertices.size();
+    int n_vertices = vertex_indices.size();
     int n_edges = (n_vertices*(n_vertices-1))/2;
     
     // we are going to number the vertices 0,1,2....,n-1 based on the order they appear in vertices
     //int vertex_to_index[this->num_vertices];
-    int index_to_vertex[n_vertices];
-
-    for(int i = 0; i<n_vertices; i++){
-        index_to_vertex[i] = vertices[i]->get_index();
-        //vertex_to_index[vertices[i]->get_index()] = i;
-    }
-
-    int vertex_i, vertex_j;
+    
+    int vertex_i_index, vertex_j_index;
     PerfectMatching *pm = new PerfectMatching(n_vertices, n_edges);
     struct PerfectMatching::Options options;
 
     for(int i = 0; i<n_vertices; i++){
         for(int j = i+1; j<n_vertices; j++){
-            vertex_i = index_to_vertex[i];
-            vertex_j = index_to_vertex[j];
-            pm->AddEdge(i,j,this->adjacency_list[vertex_i][vertex_j]->get_weight());
+            vertex_i_index = vertex_indices[i];
+            vertex_j_index = vertex_indices[j];
+            pm->AddEdge(i,j,this->adjacency_list[vertex_i_index][vertex_j_index]->get_weight());
         }
     }
 
@@ -37,13 +31,13 @@ std::vector<std::pair<int,int>> Graph::perfect_mincost_matching(std::vector<Vert
     for(index1 = 0; index1<n_vertices; index1++){
         index2 = pm->GetMatch(index1);
         if(index1 > index2){
-            v_index1 = index_to_vertex[index1];
-            v_index2 = index_to_vertex[index2];
+            v_index1 = vertex_indices[index1];
+            v_index2 = vertex_indices[index2];
             solution.push_back(std::make_pair(v_index1, v_index2));
         }
     }
 
     delete pm;
-    
+
     return solution;
 }
