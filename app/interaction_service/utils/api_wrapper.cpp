3972c9b36c5af0f31f7ca0927b4cdda9b77cc4ab
Pylyr <avadakidabra.magic@gmail.com>
2021-11-25 03:04:02 +0100

    Added a header for the wrapper

--- app/interaction_service/utils/api_wrapper.cpp
+++ app/interaction_service/utils/api_wrapper.cpp
@@ -1,74 +1,64 @@
-#include <QCoreApplication>
-#include <QFile>
-#include <QHttpMultiPart>
-#include <QNetworkAccessManager>
-#include <QNetworkReply>
-#include <iostream>
+#include "api_wrapper.h"
 
-class OpenStreetMapWrapper: public QObject{
-		Q_OBJECT
-public:
-		OpenStreetMapWrapper(QObject *parent=nullptr):QObject(parent){
-				connect(&manager, &QNetworkAccessManager::finished, this, &OpenStreetMapWrapper::handle_finished);
-		}
+OpenStreetMapWrapper::OpenStreetMapWrapper(QObject *parent):QObject(parent){
+		connect(&manager, &QNetworkAccessManager::finished, this, &OpenStreetMapWrapper::handle_finished);
+}
 
-		void download(const std::string &region, const std::string &department, const QFile& outfile){
-				QNetworkRequest request;
-				QUrl url = QUrl(QString::fromStdString("https://download.openstreetmap.fr/extracts/europe/france/" + region + "/" + department + ".osm.pbf"));
-				request.setUrl(url);
-				request.setAttribute(QNetworkRequest::User, outfile.fileName());
-				manager.get(request);
-		}
+void OpenStreetMapWrapper::download(const std::string &region, const std::string &department, const QFile& outfile){
+		QNetworkRequest request;
+		QUrl url = QUrl(QString::fromStdString("https://download.openstreetmap.fr/extracts/europe/france/" + region + "/" + department + ".osm.pbf"));
+		request.setUrl(url);
+		request.setAttribute(QNetworkRequest::User, outfile.fileName());
+		manager.get(request);
+}
 
-		void searchCSV(QFile& file, QFile& outfile){
-				QNetworkRequest request(QUrl("https://api-adresse.data.gouv.fr/search/csv/")); // Free API provided by the French government
-				request.setAttribute(QNetworkRequest::User, outfile.fileName());
-				QHttpMultiPart *multipart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
+void OpenStreetMapWrapper::searchCSV(QFile& file, QFile& outfile){
+		QNetworkRequest request(QUrl("https://api-adresse.data.gouv.fr/search/csv/")); // Free API provided by the French government
+		request.setAttribute(QNetworkRequest::User, outfile.fileName());
+		QHttpMultiPart *multipart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
 
-				QHttpPart postpart;
-				postpart.setHeader(QNetworkRequest::ContentDispositionHeader,
-													 QString("form-data; name=%1; filename=%2")
-													 .arg("data", file.fileName()));
-				postpart.setBodyDevice(&file);
-				multipart->append(postpart);
+		QHttpPart postpart;
+		postpart.setHeader(QNetworkRequest::ContentDispositionHeader,
+											 QString("form-data; name=%1; filename=%2")
+											 .arg("data", file.fileName()));
+		postpart.setBodyDevice(&file);
+		multipart->append(postpart);
 
-				QHttpPart longitude, latitude, addresse;
+		QHttpPart longitude, latitude, addresse;
 
-				longitude.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=result_columns"));
-				longitude.setBody("longitude");
-				multipart->append(longitude);
+		longitude.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=result_columns"));
+		longitude.setBody("longitude");
+		multipart->append(longitude);
 
-				latitude.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=result_columns"));
-				latitude.setBody("latitude");
-				multipart->append(latitude);
+		latitude.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=result_columns"));
+		latitude.setBody("latitude");
+		multipart->append(latitude);
 
-				latitude.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=columns"));
-				latitude.setBody("adresse");
-				multipart->append(latitude);
+		latitude.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=columns"));
+		latitude.setBody("adresse");
+		multipart->append(latitude);
 
-				file.setParent(multipart);
-				manager.post(request, multipart);
-		}
+		file.setParent(multipart);
+		manager.post(request, multipart);
+}
 
-private:
-		QNetworkAccessManager manager;
-		void handle_finished(QNetworkReply *reply){
-				if(reply->error() == QNetworkReply::NoError){
-						QByteArray read = reply->readAll();
-						QString filename = reply->request().attribute(QNetworkRequest::User).toString();
-						QFile out(filename);
-						if(out.open(QIODevice::WriteOnly)){
-								out.write(read);
-								out.close();
-						}
+void OpenStreetMapWrapper::handle_finished(QNetworkReply *reply){
+		if(reply->error() == QNetworkReply::NoError){
+				QByteArray read = reply->readAll();
+				QString filename = reply->request().attribute(QNetworkRequest::User).toString();
+				QFile out(filename);
+				if(out.open(QIODevice::WriteOnly)){
+						out.write(read);
+						out.close();
 				}
-				else{
-						qDebug() << reply->error() << reply->errorString();
-				}
-				reply->deleteLater();
+		}
+		else{
+				qDebug() << reply->error() << reply->errorString();
+		}
+		reply->deleteLater();
 //				QCoreApplication::quit(); You need to implement it
-		}
-};
+}
+
 #include "api_wrapper.moc"
 
 /*
