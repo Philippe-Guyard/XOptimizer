f3fbf30490d3cbce81f597403f58856b35ddc4e6
AntonioDaSilva <mertunsal1905@gmail.com>
2021-11-25 18:26:52 +0300

    brute force nearest neighbor and map hpp hotfix

--- core-algo/temp/map.hpp
+++ core-algo/temp/map.hpp
@@ -5,11 +5,8 @@ class Map{
     public:
 
         Map(int num_vertices, VertexData* vertex_data_array, std::vector<std::vector<std::pair<int, EdgeWeight>>> adjacency_list);
-        EdgeWeight get_edge_weight(Vertex* v1, Vertex* v2);
-        EdgeWeight get_edge_weight(int i, int j);
-        EdgeWeight get_edge_weight(Vertex* v1, int j);
-        EdgeWeight get_edge_weight(int i, Vertex* v2);
-        int brute_force_closest_vertex_index(VertexData* vertex_data);
+        int brute_force_closest_vertex_index(VertexData& vertex_data);
+        
 
 
     protected:
@@ -23,7 +20,9 @@ class Map{
         std::unordered_map< VertexData, int, std::hash<VertexData> > vertex_position;
         std::vector<std::unordered_map<int, EdgeWeight>> edge_weights;
 
-
-
+        EdgeWeight get_edge_weight(Vertex* v1, Vertex* v2);
+        EdgeWeight get_edge_weight(int i, int j);
+        EdgeWeight get_edge_weight(Vertex* v1, int j);
+        EdgeWeight get_edge_weight(int i, Vertex* v2);  
 
 };
