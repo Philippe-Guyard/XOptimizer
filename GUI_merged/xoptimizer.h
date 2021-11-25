abd5a150b901f3aa821cde57806a2b1572ca688d
nossaiba-kheiri <92287276+nossaiba-kheiri@users.noreply.github.com>
2021-11-25 10:18:58 +0100

    Route visualiser and ui merged

---
+++ GUI_merged/xoptimizer.h
@@ -0,0 +1,32 @@
+#ifndef XOPTIMIZER_H
+#define XOPTIMIZER_H
+#include <QStandardItemModel>
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class XOptimizer; }
+QT_END_NAMESPACE
+
+class XOptimizer : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    XOptimizer(QWidget *parent = nullptr);
+    ~XOptimizer();
+
+private slots:
+    void on_DepartmentcomboBox_currentTextChanged(const QString &arg1);
+
+    void on_uploadFileButton_clicked();
+
+    void on_pushButton_clicked();
+
+    void saveFile(const QString &name);
+
+    void on_CitycomboBox_2_currentTextChanged(const QString &arg1);
+
+private:
+    Ui::XOptimizer *ui;
+};
+#endif // XOPTIMIZER_H
