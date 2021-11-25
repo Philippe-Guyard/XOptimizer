218e4d77959d7b54bcc274d58cc7cb87c9c0368b
AntonioDaSilva <mertunsal1905@gmail.com>
2021-11-25 14:25:26 +0300

    map class update

--- core-algo/temp/map.cpp
+++ core-algo/temp/map.cpp
@@ -1,5 +1,4 @@
 #include "map.hpp"
-#include "../src/graph.hpp"
 
 Map::Map(int num_vertices, VertexData* vertex_data_array, std::vector<std::vector<std::pair<int, EdgeWeight>>> adjacency_list){
 
@@ -7,6 +6,10 @@ Map::Map(int num_vertices, VertexData* vertex_data_array, std::vector<std::vecto
     this->num_vertices = num_vertices;
     this->num_edges = 0;
 
+    // sort the vertex data array by geolocations 
+    // std::sort(vertex_data_array, vertex_data_array + num_vertices, [](VertexData* ptr1, VertexData* ptr2) {return ptr1->get_geolocation() < ptr2->get_geolocation();});
+
+    // create unordered maps for every vertex for O(1) access to weights
     this->edge_weights.resize(num_vertices);
 
     for(int i=0; i<num_vertices; ++i){
@@ -18,10 +21,10 @@ Map::Map(int num_vertices, VertexData* vertex_data_array, std::vector<std::vecto
     for(int i=0; i<num_vertices; ++i){
         vertex_position[vertex_data_array[i]] = i;
     }
-
     
     int vertex_ind;
     EdgeWeight edge_weight;
+    int position;
     for(int i = 0; i<num_vertices; ++i){
         for(int j = 0; j<adjacency_list[i].size(); ++j){
 
@@ -35,9 +38,47 @@ Map::Map(int num_vertices, VertexData* vertex_data_array, std::vector<std::vecto
             this->num_edges++;
 
             this->adjacency_list[i].push_back(new_edge_ptr);
-            this->edge_weights[i][vertex_data_array[j]] = edge_weight;
+            position = vertex_position[vertex_data_array[j]];
+            this->edge_weights[i][position] = edge_weight;
             
         }
     }
 
 }
+
+EdgeWeight Map::get_edge_weight(int i, int j){
+
+    if (this->edge_weights[i].count(j)){
+        return this->edge_weights[i][j];
+    }
+    return -1;
+}
+
+EdgeWeight Map::get_edge_weight(int i, Vertex* v2){
+
+    int j = v2->get_index();
+    if (this->edge_weights[i].count(j)){
+        return this->edge_weights[i][j];
+    }
+    return -1;
+}
+
+EdgeWeight Map::get_edge_weight(Vertex* v1, int j){
+
+    int i = v1->get_index();
+    if (this->edge_weights[i].count(j)){
+        return this->edge_weights[i][j];
+    }
+    return -1;
+}
+
+EdgeWeight Map::get_edge_weight(Vertex* v1, Vertex* v2){
+
+    int i = v1->get_index();
+    int j = v2->get_index();
+    if (this->edge_weights[i].count(j)){
+        return this->edge_weights[i][j];
+    }
+    return -1;
+}
+
--- core-algo/temp/map.hpp
+++ core-algo/temp/map.hpp
@@ -5,6 +5,12 @@ class Map{
     public:
 
         Map(int num_vertices, VertexData* vertex_data_array, std::vector<std::vector<std::pair<int, EdgeWeight>>> adjacency_list);
+        EdgeWeight get_edge_weight(Vertex* v1, Vertex* v2);
+        EdgeWeight get_edge_weight(int i, int j);
+        EdgeWeight get_edge_weight(Vertex* v1, int j);
+        EdgeWeight get_edge_weight(int i, Vertex* v2);
+        int brute_force_closest_vertex_index(VertexData* vertex_data);
+
 
     protected:
 
@@ -15,7 +21,7 @@ class Map{
         std::vector<Edge*> edges;
         std::vector<std::vector<Edge*>> adjacency_list;
         std::unordered_map< VertexData, int, std::hash<VertexData> > vertex_position;
-        std::vector<std::unordered_map<VertexData, EdgeWeight, std::hash<VertexData>>> edge_weights;
+        std::vector<std::unordered_map<int, EdgeWeight>> edge_weights;
 
 
 
