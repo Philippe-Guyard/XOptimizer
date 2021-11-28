817a1bb36a6f30801467dbe0068a494f221bd7d8
Philippe Guyard <philippe.guyard@outlook.com>
2021-11-28 15:26:19 +0100

    add scratch of kdtree

---
+++ playground/kdtree.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <array>
+#include <memory>
+#include <stdexcept>
+#include <cmath>
+
+
+/*
+ * Inspired by https://github.com/crvs/KDTree
+ */
+
+template<size_t k>
+class KdTreeNode {
+using KdTreeNodePtr = std::shared_ptr<KdTreeNode<k>>;
+
+private:
+    KdTreeNodePtr left, right;
+    std::array<double, k> value;
+public:
+    KdTreeNode(const std::array<double, k>& value, KdTreeNodePtr left, KdTreeNodePtr right) {
+        this->value = std::array<double, k>(value);
+        this->left = left;
+        this->right = right;
+    }
+
+    KdTreeNode(const std::array<double, k>& value) : KdTreeNode(value, nullptr, nullptr) {}
+
+    double coord_at(int index) {
+        if (index < 0 || index >= k)
+            throw std::invalid_argument("Invalid index for KdTreeNode");
+
+        return value[index];
+    };
+
+    double dist2(const KdTreeNode<k>& other) const {
+        double result = 0;
+        for(int i = 0; i < k; ++i)
+            result += (value[i] - other.value[i]) * (value[i] - other.value[i]);
+
+        return result;
+    }
+
+    void add_children() {
+
+    }
+};
+
+class KdTree {
+private:
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
