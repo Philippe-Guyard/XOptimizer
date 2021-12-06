c969826272996d5b003d18d77e5097c2f8b9577c
gcognata <giuseppe.cognac@gmail.com>
2021-12-06 13:53:48 +0100

    Update clustering_vertices.cpp

--- core-algo/temp/clustering_vertices.cpp
+++ core-algo/temp/clustering_vertices.cpp
@@ -9,18 +9,18 @@
 #include<assert.h>
 
 #include "graph.hpp"
-#include "kmeans.cpp"
+#include "Vec3_temp.cpp"
 
 std::vector< std::vector<int> >Graph::cluster_vertices(const std::vector<int> &vertices_to_cluster, int num_clusters){
 
-    std::vector< std::pair<double, double> > vertex_coordinates;
+    ArrayVec3 vertex_coordinates;
 
     for(auto index : vertices_to_cluster){
         vertex_coordinates.push_back( vertices[index]->get_eucliean_coordinates() );
     }
 
     
-    std::vector< std::vector<int> > clusters = k_means(vertex_coordinates, num_clusters);
+    std::vector< std::vector<int> > clusters = vertex_coordinates.k_means(num_clusters);
 
     return clusters;
 
