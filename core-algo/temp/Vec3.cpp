fab7ed8cb33b112d28b507c7a06343ad9ab1d936
Pedro Cabral <pedro.cabral@polytechnique.edu>
2021-12-06 23:01:27 +0100

    Separated Vec3 into hpp and cpp

--- core-algo/temp/Vec3.cpp
+++ core-algo/temp/Vec3.cpp
@@ -1,52 +1,5 @@
-#include<iostream>
-#include<math.h>
-#include<vector>
-#include<assert.h>
+#include "Vec3.hpp"
 
-#include<cstdio>
-#include<time.h>
-#include<math.h>
-
-#include<random>
-
-#include<algorithm>
-
-double R_earth = 6371000; // in m
-
-class Vec3{
-
-public:
-
-    Vec3();
-    Vec3(double x, double y, double z);
-
-    Vec3(double theta, double phi);
-
-    ~Vec3();
-
-    double get_x() const;
-    double get_y() const;
-    double get_z() const;
-
-    void set_x(double x);
-    void set_y(double y);
-    void set_z(double z);
-
-    double norm2() const;
-    double norm() const;
-
-
-    // OPERATORS
-
-    Vec3 operator+(const Vec3& u)   const;
-    Vec3 operator-(const Vec3& u)   const;
-    Vec3 operator-()    const;
-
-protected:
-
-    double x,y,z;
-
-};
 
 Vec3::Vec3(double x, double y, double z){
     this->x = x;
@@ -56,6 +9,10 @@ Vec3::Vec3(double x, double y, double z){
 
 // Done assuming the angles are in degrees, if radians replace with angle*180/pi
 Vec3::Vec3(double theta, double phi){
+    
+    // Later change this to a static attribute
+    double R_earth = 6371000; // in m
+
     x = R_earth*sin(phi)*cos(theta);
     y = R_earth*sin(phi)*sin(theta);
     z = R_earth*cos(phi);
@@ -137,63 +94,12 @@ Vec3 Vec3::operator-()    const{
 }
 
 
-
-template<typename R>
-Vec3 operator*(const Vec3& v, R r){
-    return Vec3(r*v.get_x(), r*v.get_y(), r*v.get_z());
-}
-
-template<typename R>
-Vec3 operator*(R r, const Vec3& v){
-    return v*r;
-}
-
-template<typename R>
-Vec3 operator/(const Vec3& v, R r){
-    if(r < 1e-7){
-        throw std::overflow_error("Divide by 0 exception.");
-    }
-
-    return Vec3(v.get_x()/r, v.get_y()/r, v.get_z()/r);
-}
-
 /**
 
 ArrayVec3 class
 
 **/
 
-class ArrayVec3{
-
-public:
-    ArrayVec3();
-    ArrayVec3(std::vector<Vec3> points);
-    ~ArrayVec3();
-
-    Vec3 operator[](int i)   const;
-    void set_entry(int i, Vec3 v);
-
-    int size()  const;
-
-    void clear();
-    void push_back(Vec3 v);
-
-    // Methods for clustering
-    Vec3 center_of_mass()   const;
-    double cost_of_array()   const;
-
-    // Helper Functions
-    int find_closest_point(Vec3 v)  const;
-
-
-    // Clustering
-    std::vector< std::vector<int> > k_means(int num_clusters)    const;
-
-
-protected:
-    std::vector<Vec3> points;
-
-};
 
 ArrayVec3::ArrayVec3(){
 }
