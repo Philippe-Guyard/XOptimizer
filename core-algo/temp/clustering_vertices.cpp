4fd67d2816b233fe00195c15c5be8560cb06b425
gcognata <giuseppe.cognac@gmail.com>
2021-12-06 13:58:34 +0100

    Update clustering_vertices.cpp

--- core-algo/temp/clustering_vertices.cpp
+++ core-algo/temp/clustering_vertices.cpp
@@ -25,36 +25,3 @@ std::vector< std::vector<int> >Graph::cluster_vertices(const std::vector<int> &v
     return clusters;
 
 }
-
-
-std::pair<double, double> Graph::find_cluster_center(const std::vector<int> &cluster){
-
-    std::vector< std::pair<double, double> > vertex_coordinates; 
-
-    for(auto index : cluster){
-        vertex_coordinates.push_back( vertices[index]->get_eucliean_coordinates() );
-    }
-
-    std::pair<double, double> mu = {0, 0};
-
-    for(const auto& point : vertex_coordinates){
-        mu.first += point.first;
-        mu.second += point.second;
-    }
-
-    mu.first = mu.first / (double) cluster.size();
-    mu.second = mu.second / (double) cluster.size();
-
-    return mu;
-
-}
-
-std::vector< std::pair<double,double> > Graph::find_clusters_centers(const std::vector< std::vector<int> > &clusters){
-
-    std::vector< std::pair< EdgeWeight, EdgeWeight > > centers;
-    for(const auto& cluster : clusters){
-        centers.push_back( find_cluster_center(cluster) );
-    }
-
-    return centers;
-}
