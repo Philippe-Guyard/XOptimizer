424a0b9894d0b70cb80b37eba04030f9db133864
Pylyr <avadakidabra.magic@gmail.com>
2021-12-13 22:36:44 +0100

    O(nlogn) A* optimisations

--- core-algo/temp/map_find_distance_astar.cpp
+++ core-algo/temp/map_find_distance_astar.cpp
@@ -1,26 +1,39 @@
 #include "map.hpp"
+#include <set>
 
 EdgeWeight Map::find_distances_astar(int start, int end){
+
+	/*
+	Finds the cost to go from vertice i to vertice j.
+
+	PARAMETERS:
+	int i, int j:
+	Indexes of the start and finish vertices, respectively.
+
+	RETURN:
+	EdgeWeight:
+	The (approximate) lowest cost to go from i to j.
+	The heuristic function is the haversine distance implemented in the VertexData class.
+
+	*/
+
 	VertexData end_data = vertices.at(end)->get_data();
 	std::vector<EdgeWeight> gScore(this->num_vertices, std::numeric_limits<EdgeWeight>::max());
 	std::vector<EdgeWeight> fScore(this->num_vertices, std::numeric_limits<EdgeWeight>::max());
 	gScore[start] = 0;
 	fScore[start] = gScore[start] + vertices.at(start)->get_data().get_distance(end_data);
 
-	std::unordered_set<int> OpenSet;
+	auto compare = [&start, &gScore](const int &a, const int &b){
+					return gScore[a] < gScore[b];
+			};
+
+	std::set<int, decltype(compare)> OpenSet(compare);
 	OpenSet.insert(start);
+
 	int cur;
-	int best_cand;
-	EdgeWeight best_cand_dist;
 
 	while (!OpenSet.empty()){
-		best_cand_dist = std::numeric_limits<EdgeWeight>::max();
-		for (int node : OpenSet){
-			if (fScore[node] < best_cand_dist){
-				best_cand = node;
-			}
-		}
-		cur = best_cand;
+		cur = *OpenSet.begin();
 		if (cur == end){
 			return fScore[end];
 		}
@@ -32,9 +45,10 @@ EdgeWeight Map::find_distances_astar(int start, int end){
 			if (gScore[to] > gScore[cur] + length){
 				gScore[to] = gScore[cur] + length;
 				fScore[to] = gScore[to] + vertices.at(to)->get_data().get_distance(end_data);
-				if (OpenSet.count(to) == 0){
-					OpenSet.insert(to);
+				if (OpenSet.count(to) > 0){
+					OpenSet.erase(to);
 				}
+				OpenSet.insert(to);
 			}
 		}
 	}
