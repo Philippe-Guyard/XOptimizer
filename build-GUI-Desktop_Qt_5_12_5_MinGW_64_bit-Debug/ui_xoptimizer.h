b93d63531b10f19daa1cb4793b0ee6361ef842ce
vilius-tubinas <gurelis321@gmail.com>
2021-12-28 17:00:39 +0200

    Made design responsive
    Using vertical and horizontal layouts made it so that the window is responsive a.k.a. when you shrink the window, the content shrinks with it.

--- build-GUI-Desktop_Qt_5_12_5_MinGW_64_bit-Debug/ui_xoptimizer.h
+++ build-GUI-Desktop_Qt_5_12_5_MinGW_64_bit-Debug/ui_xoptimizer.h
@@ -12,11 +12,13 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QComboBox>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStackedWidget>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTableWidget>
@@ -29,28 +31,38 @@ class Ui_XOptimizer
 {
 public:
     QWidget *centralwidget;
+    QVBoxLayout *verticalLayout_10;
     QStackedWidget *stackedWidget;
     QWidget *page_3;
-    QWidget *layoutWidget;
-    QVBoxLayout *verticalLayout_2;
-    QLabel *label_2;
-    QComboBox *CitycomboBox_2;
-    QWidget *layoutWidget_2;
+    QVBoxLayout *verticalLayout_5;
+    QVBoxLayout *verticalLayout_4;
+    QLabel *label_3;
     QVBoxLayout *verticalLayout;
     QLabel *label;
     QComboBox *DepartmentcomboBox;
-    QLabel *label_3;
+    QVBoxLayout *verticalLayout_2;
+    QLabel *label_2;
+    QComboBox *CitycomboBox_2;
+    QVBoxLayout *verticalLayout_11;
+    QSpacerItem *verticalSpacer;
     QPushButton *NextButton;
     QWidget *page_2;
+    QVBoxLayout *verticalLayout_7;
+    QVBoxLayout *verticalLayout_9;
+    QSpacerItem *verticalSpacer_3;
+    QHBoxLayout *horizontalLayout;
     QLabel *loaderlabel;
     QLabel *label_4;
+    QSpacerItem *verticalSpacer_2;
     QWidget *page_4;
-    QPushButton *pushButton;
-    QTableWidget *tableWidget;
-    QWidget *layoutWidget_3;
+    QVBoxLayout *verticalLayout_8;
+    QVBoxLayout *verticalLayout_6;
+    QHBoxLayout *horizontalLayout_4;
     QVBoxLayout *verticalLayout_3;
     QLabel *filelabel;
     QPushButton *uploadFileButton;
+    QPushButton *pushButton;
+    QTableWidget *tableWidget;
     QWidget *page;
     QWidget *page_5;
     QMenuBar *menubar;
@@ -60,95 +72,172 @@ public:
     {
         if (XOptimizer->objectName().isEmpty())
             XOptimizer->setObjectName(QString::fromUtf8("XOptimizer"));
-        XOptimizer->resize(800, 600);
+        XOptimizer->resize(820, 590);
         centralwidget = new QWidget(XOptimizer);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayout_10 = new QVBoxLayout(centralwidget);
+        verticalLayout_10->setObjectName(QString::fromUtf8("verticalLayout_10"));
         stackedWidget = new QStackedWidget(centralwidget);
         stackedWidget->setObjectName(QString::fromUtf8("stackedWidget"));
-        stackedWidget->setGeometry(QRect(0, 0, 801, 571));
         page_3 = new QWidget();
         page_3->setObjectName(QString::fromUtf8("page_3"));
-        layoutWidget = new QWidget(page_3);
-        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
-        layoutWidget->setGeometry(QRect(240, 260, 341, 71));
-        verticalLayout_2 = new QVBoxLayout(layoutWidget);
-        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
-        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
-        label_2 = new QLabel(layoutWidget);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
-
-        verticalLayout_2->addWidget(label_2);
-
-        CitycomboBox_2 = new QComboBox(layoutWidget);
-        CitycomboBox_2->setObjectName(QString::fromUtf8("CitycomboBox_2"));
-
-        verticalLayout_2->addWidget(CitycomboBox_2);
-
-        layoutWidget_2 = new QWidget(page_3);
-        layoutWidget_2->setObjectName(QString::fromUtf8("layoutWidget_2"));
-        layoutWidget_2->setGeometry(QRect(240, 190, 341, 70));
-        verticalLayout = new QVBoxLayout(layoutWidget_2);
-        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
-        verticalLayout->setContentsMargins(0, 0, 0, 0);
-        label = new QLabel(layoutWidget_2);
-        label->setObjectName(QString::fromUtf8("label"));
-
-        verticalLayout->addWidget(label);
-
-        DepartmentcomboBox = new QComboBox(layoutWidget_2);
-        DepartmentcomboBox->setObjectName(QString::fromUtf8("DepartmentcomboBox"));
-
-        verticalLayout->addWidget(DepartmentcomboBox);
-
+        verticalLayout_5 = new QVBoxLayout(page_3);
+        verticalLayout_5->setObjectName(QString::fromUtf8("verticalLayout_5"));
+        verticalLayout_5->setContentsMargins(50, -1, 50, -1);
+        verticalLayout_4 = new QVBoxLayout();
+        verticalLayout_4->setObjectName(QString::fromUtf8("verticalLayout_4"));
         label_3 = new QLabel(page_3);
         label_3->setObjectName(QString::fromUtf8("label_3"));
-        label_3->setGeometry(QRect(240, 80, 341, 41));
         QFont font;
         font.setPointSize(16);
         font.setBold(false);
         font.setWeight(50);
         label_3->setFont(font);
         label_3->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_4->addWidget(label_3);
+
+
+        verticalLayout_5->addLayout(verticalLayout_4);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        label = new QLabel(page_3);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        verticalLayout->addWidget(label);
+
+        DepartmentcomboBox = new QComboBox(page_3);
+        DepartmentcomboBox->setObjectName(QString::fromUtf8("DepartmentcomboBox"));
+
+        verticalLayout->addWidget(DepartmentcomboBox);
+
+
+        verticalLayout_5->addLayout(verticalLayout);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        label_2 = new QLabel(page_3);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        verticalLayout_2->addWidget(label_2);
+
+        CitycomboBox_2 = new QComboBox(page_3);
+        CitycomboBox_2->setObjectName(QString::fromUtf8("CitycomboBox_2"));
+
+        verticalLayout_2->addWidget(CitycomboBox_2);
+
+
+        verticalLayout_5->addLayout(verticalLayout_2);
+
+        verticalLayout_11 = new QVBoxLayout();
+        verticalLayout_11->setObjectName(QString::fromUtf8("verticalLayout_11"));
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_11->addItem(verticalSpacer);
+
         NextButton = new QPushButton(page_3);
         NextButton->setObjectName(QString::fromUtf8("NextButton"));
-        NextButton->setGeometry(QRect(240, 400, 341, 31));
+
+        verticalLayout_11->addWidget(NextButton);
+
+
+        verticalLayout_5->addLayout(verticalLayout_11);
+
+        verticalLayout_5->setStretch(0, 1);
+        verticalLayout_5->setStretch(1, 1);
+        verticalLayout_5->setStretch(2, 1);
+        verticalLayout_5->setStretch(3, 3);
         stackedWidget->addWidget(page_3);
         page_2 = new QWidget();
         page_2->setObjectName(QString::fromUtf8("page_2"));
+        verticalLayout_7 = new QVBoxLayout(page_2);
+        verticalLayout_7->setObjectName(QString::fromUtf8("verticalLayout_7"));
+        verticalLayout_9 = new QVBoxLayout();
+        verticalLayout_9->setObjectName(QString::fromUtf8("verticalLayout_9"));
+        verticalSpacer_3 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_9->addItem(verticalSpacer_3);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
         loaderlabel = new QLabel(page_2);
         loaderlabel->setObjectName(QString::fromUtf8("loaderlabel"));
-        loaderlabel->setGeometry(QRect(350, 210, 81, 81));
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(loaderlabel->sizePolicy().hasHeightForWidth());
+        loaderlabel->setSizePolicy(sizePolicy);
+        loaderlabel->setMaximumSize(QSize(100, 100));
+        loaderlabel->setLayoutDirection(Qt::LeftToRight);
         loaderlabel->setAutoFillBackground(false);
         loaderlabel->setScaledContents(true);
+        loaderlabel->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(loaderlabel);
+
+
+        verticalLayout_9->addLayout(horizontalLayout);
+
         label_4 = new QLabel(page_2);
         label_4->setObjectName(QString::fromUtf8("label_4"));
-        label_4->setGeometry(QRect(320, 310, 141, 20));
         label_4->setAutoFillBackground(true);
         label_4->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_9->addWidget(label_4);
+
+        verticalSpacer_2 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_9->addItem(verticalSpacer_2);
+
+
+        verticalLayout_7->addLayout(verticalLayout_9);
+
         stackedWidget->addWidget(page_2);
         page_4 = new QWidget();
         page_4->setObjectName(QString::fromUtf8("page_4"));
-        pushButton = new QPushButton(page_4);
-        pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(410, 30, 371, 61));
-        tableWidget = new QTableWidget(page_4);
-        tableWidget->setObjectName(QString::fromUtf8("tableWidget"));
-        tableWidget->setGeometry(QRect(20, 120, 761, 421));
-        layoutWidget_3 = new QWidget(page_4);
-        layoutWidget_3->setObjectName(QString::fromUtf8("layoutWidget_3"));
-        layoutWidget_3->setGeometry(QRect(20, 30, 371, 59));
-        verticalLayout_3 = new QVBoxLayout(layoutWidget_3);
+        verticalLayout_8 = new QVBoxLayout(page_4);
+        verticalLayout_8->setObjectName(QString::fromUtf8("verticalLayout_8"));
+        verticalLayout_6 = new QVBoxLayout();
+        verticalLayout_6->setObjectName(QString::fromUtf8("verticalLayout_6"));
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        horizontalLayout_4->setContentsMargins(-1, 0, -1, -1);
+        verticalLayout_3 = new QVBoxLayout();
         verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
-        verticalLayout_3->setContentsMargins(0, 0, 0, 0);
-        filelabel = new QLabel(layoutWidget_3);
+        filelabel = new QLabel(page_4);
         filelabel->setObjectName(QString::fromUtf8("filelabel"));
 
         verticalLayout_3->addWidget(filelabel);
 
-        uploadFileButton = new QPushButton(layoutWidget_3);
+        uploadFileButton = new QPushButton(page_4);
         uploadFileButton->setObjectName(QString::fromUtf8("uploadFileButton"));
 
         verticalLayout_3->addWidget(uploadFileButton);
+
+
+        horizontalLayout_4->addLayout(verticalLayout_3);
+
+        pushButton = new QPushButton(page_4);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        QSizePolicy sizePolicy1(QSizePolicy::Minimum, QSizePolicy::Minimum);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(pushButton->sizePolicy().hasHeightForWidth());
+        pushButton->setSizePolicy(sizePolicy1);
+
+        horizontalLayout_4->addWidget(pushButton);
+
+
+        verticalLayout_6->addLayout(horizontalLayout_4);
+
+        tableWidget = new QTableWidget(page_4);
+        tableWidget->setObjectName(QString::fromUtf8("tableWidget"));
+
+        verticalLayout_6->addWidget(tableWidget);
+
+
+        verticalLayout_8->addLayout(verticalLayout_6);
 
         stackedWidget->addWidget(page_4);
         page = new QWidget();
@@ -157,10 +246,13 @@ public:
         page_5 = new QWidget();
         page_5->setObjectName(QString::fromUtf8("page_5"));
         stackedWidget->addWidget(page_5);
+
+        verticalLayout_10->addWidget(stackedWidget);
+
         XOptimizer->setCentralWidget(centralwidget);
         menubar = new QMenuBar(XOptimizer);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 26));
+        menubar->setGeometry(QRect(0, 0, 820, 26));
         XOptimizer->setMenuBar(menubar);
         statusbar = new QStatusBar(XOptimizer);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -168,7 +260,7 @@ public:
 
         retranslateUi(XOptimizer);
 
-        stackedWidget->setCurrentIndex(0);
+        stackedWidget->setCurrentIndex(2);
 
 
         QMetaObject::connectSlotsByName(XOptimizer);
@@ -177,15 +269,15 @@ public:
     void retranslateUi(QMainWindow *XOptimizer)
     {
         XOptimizer->setWindowTitle(QApplication::translate("XOptimizer", "XOptimizer", nullptr));
-        label_2->setText(QApplication::translate("XOptimizer", "Choose your city:", nullptr));
-        label->setText(QApplication::translate("XOptimizer", "Choose your department:", nullptr));
         label_3->setText(QApplication::translate("XOptimizer", "WELCOME TO XOPTIMIZER", nullptr));
+        label->setText(QApplication::translate("XOptimizer", "Choose your department:", nullptr));
+        label_2->setText(QApplication::translate("XOptimizer", "Choose your city:", nullptr));
         NextButton->setText(QApplication::translate("XOptimizer", "Next", nullptr));
         loaderlabel->setText(QString());
         label_4->setText(QApplication::translate("XOptimizer", "Fetching data...", nullptr));
-        pushButton->setText(QApplication::translate("XOptimizer", "Submit and optimize", nullptr));
         filelabel->setText(QApplication::translate("XOptimizer", "No file chosen. Click the button below to upload", nullptr));
         uploadFileButton->setText(QApplication::translate("XOptimizer", "Upload file", nullptr));
+        pushButton->setText(QApplication::translate("XOptimizer", "Submit and optimize", nullptr));
     } // retranslateUi
 
 };
